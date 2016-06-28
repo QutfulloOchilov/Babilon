b625087a974e6fa42bff9882a4f28c24ddd55f77
Qutfullo <Qutfullo@DESKTOP-J04NUEP>
2016-06-28 15:12:14 +0500

    Add feature message and created CustomUserControl

--- Babilon/MainWindow.xaml.cs
+++ Babilon/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Babilon.Message;
 namespace Babilon
 {
     /// <summary>
@@ -27,10 +27,54 @@ namespace Babilon
         public MainWindow()
         {
             InitializeComponent();
-            viewModel = new ViewModel();
-            viewModel.CurrentContent.Content = new MainPage();
-            viewModel.CurrentContent.DataContext = viewModel;
-            this.DataContext = viewModel;
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MessageManager.ShowMessage += MessageManager_ShowMessage;
+                viewModel = new ViewModel();
+                viewModel.CurrentContent.Content = new MainPage();
+                viewModel.CurrentContent.DataContext = viewModel;
+                this.DataContext = viewModel;
+            }
+            catch (Exception ex)
+            {
+                var iii = ex;
+                
+            }
+        }
+
+        private async void MessageManager_ShowMessage(MessageManagerEventArgs e)
+        {
+            var message = e.Message;
+            var messageSetting = new MetroDialogSettings();
+            var messageDialogStyle = MessageDialogStyle.Affirmative;
+            switch (message.Choices.Count)
+            {
+                case 1:
+                    messageSetting.AffirmativeButtonText = message.Choices.Single().Title;
+                    break;
+                case 2:
+                    messageSetting.AffirmativeButtonText = message.Choices[0].Title;
+                    messageSetting.NegativeButtonText = message.Choices[1].Title;
+                    messageDialogStyle = MessageDialogStyle.AffirmativeAndNegative;
+                    break;
+                case 3:
+                    messageSetting.AffirmativeButtonText = message.Choices[0].Title;
+                    messageSetting.NegativeButtonText = message.Choices[1].Title;
+                    messageSetting.FirstAuxiliaryButtonText = message.Choices[2].Title;
+                    messageDialogStyle = MessageDialogStyle.AffirmativeAndNegativeAndDoubleAuxiliary;
+                    break;
+                default:
+                    break;
+            }
+
+            await this.ShowMessageAsync(message.Title, message.Detail,
+                messageDialogStyle, messageSetting);
         }
 
         private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
